e664e7f491be61540f10f25dcfe203dff5729277
N1chegons <krikignatenko@gmail.com>
2026-05-07 19:58:15 +0500

    Upgraded max logic

--- src/max/bot.py
+++ src/max/bot.py
@@ -34,9 +34,8 @@ async def new_session(event: MessageCreated):
     user_id = event.from_user.user_id
     user_reg = await MaxService.get_user(user_id)
     await MaxService.delete_session(user_reg.user_id)
-
     await MaxService.create_session(user_reg.user_id)
-    await MaxService.update_user_state(user_reg, UserState.NEW)
+    await MaxService.update_user_state(user_id, UserState.NEW)
 
     reply_kb = InlineKeyboardBuilder()
     reply_kb.row(
