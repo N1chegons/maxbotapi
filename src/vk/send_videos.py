9118dbbbf508fa2914ee3dac8283e988690f1a95
N1chegons <krikignatenko@gmail.com>
2026-05-10 12:27:18 +0500

    add link sending

--- src/vk/send_videos.py
+++ src/vk/send_videos.py
@@ -1,5 +1,14 @@
 #!/home/psylogic/maxapibotnew/venv/bin/python
+import sys
+import os
+
+# Добавляем путь к корню проекта
+project_root = '/home/psylogic/maxapibotnew'
+sys.path.insert(0, project_root)
+
+# Теперь импорты будут работать
 from src.vk.repository import VkIntegrationNew
 
-vk = VkIntegrationNew()
-vk.send_random_video()
+if __name__ == "__main__":
+    vk = VkIntegrationNew()
+    vk.send_random_video()
--- src/vk/update_videos.py
+++ src/vk/update_videos.py
@@ -1,5 +1,13 @@
 #!/home/psylogic/maxapibotnew/venv/bin/python
+import sys
+import os
+
+# Добавляем путь к корню проекта
+project_root = '/home/psylogic/maxapibotnew'
+sys.path.insert(0, project_root)
+
 from src.vk.repository import VkIntegrationNew
 
-vk = VkIntegrationNew()
-vk.update_video_links()
+if __name__ == "__main__":
+    vk = VkIntegrationNew()
+    vk.update_video_links()
