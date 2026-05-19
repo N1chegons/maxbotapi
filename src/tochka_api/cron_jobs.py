dfe46ceee4fb439c764784bf74b68f43b058971b
N1chegons <krikignatenko@gmail.com>
2026-05-19 15:28:05 +0500

    bvs

--- src/tochka_api/cron_jobs.py
+++ src/tochka_api/cron_jobs.py
@@ -1,10 +1,21 @@
 import asyncio
 import sys
+import logging
 
 project_root = '/home/psylogic/maxapibotnew'
 sys.path.insert(0, project_root)
 
 from src.tochka_api.auto_payment import auto_charge_active_subscriptions, auto_charge_after_trial
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('/home/psylogic/maxapibotnew/logs/auto_charge.log'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 
 async def main():
     await auto_charge_active_subscriptions()
